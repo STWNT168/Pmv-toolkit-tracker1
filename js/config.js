d536c7b5dc2c78ec23ee0966a7c7c23fc2815c93
STWNT168 <SATWANT168@GMAIL.COM>
2026-08-18 08:37:00 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+/* PMV Toolkit Tracker v8.1
+ * Frontend configuration.
+ * Keep SPREADSHEET_ID only in Apps Script Script Properties.
+ */
+window.PMV_CONFIG = Object.freeze({
+  APP_NAME: 'PMV Toolkit Tracker',
+  APP_SCRIPT_API_URL: 'https://script.google.com/macros/s/AKfycbzdTwEfyuolUkyr9ME0_4u4iQmED25GMvyqqBcHFFPwehX355nCwn4jFhbmF7ILVHaD/exec',
+  REQUEST_TIMEOUT_MS: 45000,
+  SESSION_STORAGE_KEY: 'pmvSessionV81',
+  APP_VERSION: '8.1.0'
+});
